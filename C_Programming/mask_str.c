96b30c88b39c4db40ef076f5c12f635b8a5aeacd
Ashish Jain <ash.1382@gmail.com>
2017-12-05 14:25:47 +0530

    Code to mask a substring based on regex pattern

---
+++ C_Programming/mask_str.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <regex.h>
+#include <string.h>
+
+#define MASK_STRING "*****"
+
+/*
+ * C code to mask a portion of the string matched by parenthesis enclosed pattern
+ * Run: gcc reg.c -lbsd
+ * Output:
+ *     Pattern: .* password (\S+).*
+ *     Match String: list password test123 all files
+ *     Masked String: list password ***** all files
+ */
+
+char *
+mask_str(const char *str, const char *pattern, char *mstr, size_t mstr_len)
+{
+        regex_t         regex;
+        regmatch_t      rematch[2];
+        regoff_t        rm_so, rm_eo;
+
+        if (regcomp(&regex, pattern, REG_EXTENDED) != 0) {
+                return (NULL);
+        }
+        if (regexec(&regex, str, sizeof (rematch)/sizeof (rematch[0]),
+            rematch, 0) == 0) {
+                if (rematch[1].rm_so == -1 || rematch[1].rm_eo == -1) {
+                        regfree(&regex);
+			return (NULL);
+                }
+                rm_so = rematch[1].rm_so;
+                rm_eo = rematch[1].rm_eo;
+                memcpy(mstr, str, rm_so);
+                memcpy(mstr+rm_so, MASK_STRING, strlen(MASK_STRING));
+                memcpy(mstr+rm_so+strlen(MASK_STRING), str+rm_eo,
+		    strlen(str) - rm_eo);
+                mstr[strlen(str) + strlen(MASK_STRING)] = '\0';
+        }
+        regfree(&regex);
+        return (mstr);
+}
+
+int main(void) {
+	char *match_str = "list password test123 all files";
+	char *pattern = ".* password (\\S+).*";
+	char masked_str[128];
+	char *cmd_ptr;
+
+	printf("Pattern: %s\n", pattern);
+	printf("Match String: %s\n", match_str);
+	cmd_ptr = mask_str(match_str, pattern, masked_str,
+	    sizeof (masked_str));
+	printf("Masked String: %s\n", cmd_ptr);
+	return (0);
+}
